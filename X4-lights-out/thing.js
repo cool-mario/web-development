8d47501d11f7ee772ee37de0ba08bd06360e5590
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-06-29 16:31:26 -0700

    xtra credittttttttttt

--- X4-lights-out/thing.js
+++ X4-lights-out/thing.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 
     // $("tr td+td+td").addClass("on");
 
+
+    let level = 1;
+
     // when square is clicked turn it on
     $("td").on ("click", (event) => {
         let target = $(event.target);
@@ -30,18 +33,57 @@ $(document).ready(function(){
         // bottomRow.find("td").eq(index).toggleClass("on");
 
 
-
         let allTD = $("td");
-        // let win = true;
 
         // if one of them is not on, win is false
         if (!allTD.hasClass("on")){
+            // WIN!!!!
             $("#winText").toggleClass("hidden");
+            
+
         }
-
-      
-
     });
+
+    // extra credit stuff
+
+
+
+
+    // reset
+    $("#reset").click(() => {
+        reset();
+        
+    });
+
+    function reset(){
+        $("td").removeClass("on"); // delete all the on blocks
+        if (level == 2){
+            level2();
+        } else if (level == 3){
+            level3()
+        }
+    }
+
+    function level1(){
+        reset();
+        for (let i = 1; i < 4; i++){
+            $("tr").eq(i).children().eq(2).addClass("on");
+        }
+    }
+
+    function level2(){
+        reset();
+        $("td").eq(7).addClass("on");
+        $("td").eq(11).addClass("on");
+        $("td").eq(13).addClass("on");
+        $("td").eq(17).addClass("on");
+    }
+
+    function level3(){
+        reset();
+        $("td").eq(1).addClass("on");
+    }
+
 
 
   });
