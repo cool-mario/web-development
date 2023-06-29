5f1711b5ed08a1ab0d33f4569e12c9373201c63c
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-06-28 19:23:37 -0700

    version 2 draft

---
+++ P1/v02/results.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            background-color: black;
+            color: antiquewhite;
+        }
+    </style>
+
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    var_dump($_POST);
+
+    $mathNerd = 0;
+    $chemNerd = 0;
+    $csNerd   = 0;
+    $historyNerd = 0;
+    $engineerNerd = 0;
+    $notANerd = 0;
+
+    ?>
+    
+</body>
+</html>
