d2e7d740d652891b2229cae4ee26f80aec53fc8d
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-02 15:32:43 -0700

    Yo finally done with augment 1 assignment

--- P1/v02.6/results.php
+++ P1/v02.6/results.php
@@ -7,30 +7,31 @@
         body {
             background-color: black;
             color: antiquewhite;
+            margin-left: 3%;
         }
         /* make table look better */
         th, td {
             background-color:  rgb(75,73,129);
             padding: 10px;
         }
+        /* selects the nerd level thats the highest */
+        tr.highlight > td {
+            background-color: rgb(0 139 255);
+            color: yellow;
+            font-weight: bold;
+        }
     </style>
 
     <title>Your Results</title>
 </head>
 <body>
-    <h1>Your results!</h1>
-    <br>
+    <!-- <h1>Your results!</h1> -->
     <?php
 
+    require_once("functions.php");
     // var_dump($_POST);
-
-    echo '<table>';
-    echo '<tr><th>Key</th><th>Value</th></tr>';
-
-    foreach ($_POST as $key => $value) {
-        echo '<tr><td>' . htmlspecialchars($key) . '</td><td>' . htmlspecialchars($value) . '</td></tr>';
-    }
-    echo '</table>';
+    // print_array($_POST);
+    
 
     $nerd = array(
         "math" => 0,
@@ -55,6 +56,7 @@
     $equation = $_POST["equation"];
     $youtube = $_POST["youtube"];
     $socialMedia = $_POST["socialMedia"];
+    $sports = $_POST["sports"] ?? "off"; // if the box isn't checked it doesn't exist
     $projects = $_POST["projects"];
     $pickClothes = $_POST["pickClothes"];
     $workspace = $_POST["workspace"];
@@ -73,25 +75,110 @@
     if ($sleepHours > 6){
         $nerd["not"]++;
     } else {
-        $nerd["chem"]++;
-        $nerd["math"]++;
-        $nerd["chem"]++;
-        $nerd["cs"]++;
-        $nerd["history"]++;
-        $nerd["lit"]++;
+        inc_nerd($nerd);
     }
     // 3
     $nerd[$skool]++;
     // 4
     $nerd[$equation]++;
+    // 5: youtube
+    $nerd[$youtube]++;
 
-    var_dump($nerd);
+    // 6: social media
+    if ($socialMedia == "a lot"){
+        $nerd["not"] += 2;
+    } elseif ($socialMedia == "more than 30"){
+        $nerd["not"]++;
+    } elseif ($socialMedia == "a little"){
+        $nerd = inc_nerd($nerd);
+    }
 
+    // 7 sports
+    if ($sports == "on"){
+        $nerd["not"]++;
+    } else {
+        $nerd = inc_nerd($nerd);
+    }
+
+    // 8 introvert
+    if ($projects == "group"){
+        $nerd["not"]++;
+    } else {
+        $nerd = inc_nerd($nerd);
+    }
+
+    // 9 fashion
+    if ($pickClothes == "barely"){
+        $nerd = inc_nerd($nerd,2);
+    } elseif ($pickClothes == "1min"){
+        inc_nerd($nerd);
+    } elseif ($pickClothes == "a few"){
+        $nerd["not"]++;
+    } elseif ($pickClothes == "zero"){
+        $nerd = inc_nerd($nerd,3);
+    } elseif ($pickClothes == "a lot"){
+        $nerd["not"] += 2;
+    }
+
+    // 10 workspace
+    if ($workspace == "organized"){
+        $nerd["math"]++;
+        $nerd["cs"]++;
+    } elseif ($workspace == "experiment"){
+        $nerd["chem"]++;
+        $nerd["physics"]++;
+    } elseif ($workspace == "art"){
+        $nerd["literature"]++;
+    } elseif ($workspace == "tech"){
+        $nerd["cs"]++;
+    } elseif ($workspace == "books"){
+        $nerd["literature"]++;
+        $nerd["history"]++;
+    } elseif ($workspace == "dump"){
+        // increase the highest nerd there is
+        $nerd[highest($nerd)]++;
+    }
+    
+    // sort the array so that the highest is first
+    arsort($nerd);
+    $highest = key($nerd); // the key with the highest value
+
+    $fullName = array(
+        "math" => "Math",
+        "chem" => "Chemistry",
+        "cs" => "Computer Science",
+        "physics" => "Physics",
+        "lit" => "Literature",
+        "not" => "Not a nerd",
+    );
+
+    if ($fullName[$highest] == "Not a"){
+        echo "<h1>You are not a nerd?!</h1>";
+    } else {
+        echo "<h1>You are a $fullName[$highest] nerd!</h1>";
+        echo '<img src="../nerd.jpg" alt="NERRRRD!!!" width="300" height="100" id="nerd">';
+    }
+
+    
+    echo '<br><br><table>';
+    echo '<tr><th>Nerd type</th><th>Amount of nerd</th></tr>';
+
+    foreach ($nerd as $key => $value) {
+        if ($highest == $key){ // if the element is the highest one
+            echo '<tr class="highlight"><td>' . htmlspecialchars($fullName[$key]) . '</td><td>' . htmlspecialchars($value) . '</td></tr>';
+        } else {
+            echo '<tr><td>' . htmlspecialchars($fullName[$key]) . '</td><td>' . htmlspecialchars($value) . '</td></tr>';
+        }
+        
+    }
+    echo '</table>';
 
     
 
 
     ?>
+
+    
     
 </body>
 </html>
