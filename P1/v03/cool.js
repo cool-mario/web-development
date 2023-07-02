d842336a9182267d84059a971394a9bf717a673b
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-02 16:41:22 -0700

    started work on the js

---
+++ P1/v03/cool.js
@@ -0,0 +1,28 @@
+$(document).ready(()=>{
+
+    let questions = $("form > p");
+    let inputs = $("input[type='radio']");
+
+    for (let i = 0; i < inputs.length; i++){
+        let input = inputs.eq(i);
+        console.log(input);
+
+        if (input.prop("checked")){
+            input.prev().addClass("answered");
+        }
+
+
+    }
+
+    $('input').on("click", (event) => {
+
+        let target = $(event.target);
+
+        console.log(target);
+        if (target.prop("checked")){
+            target.toggleClass("answered");
+        } 
+    });
+
+
+});
