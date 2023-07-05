07a7be94477818bc08680e79fe34053ce8a5cb2c
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-04 21:55:26 -0700

    js finished, css updated, bus squished
    the questions now look different after you answer them. css changed so it has a color theme now, kinda

--- P1/v03/cool.js
+++ P1/v03/cool.js
@@ -2,27 +2,62 @@ $(document).ready(()=>{
 
     let questions = $("form > p");
     let inputs = $("input[type='radio']");
+    let labels = $("form > div > label");
 
     // loop thru all inputs
-    for (let i = 0; i < inputs.length; i++){
-        let input = inputs.eq(i);
-        console.log(input);
+    // for (let i = 0; i < inputs.length; i++){
+    //     let input = inputs.eq(i);
+        // console.log(input);
 
         // if (input.prop("checked")){
         //     input.prev().addClass("answered");
         // }
 
 
-    }
+    // }
 
     $('input').on("click", (event) => {
 
         let target = $(event.target);
 
-        console.log(target);
         if (target.prop("checked")){
-            target.toggleClass("answered");
+            // adds class to div
+            target.parent().addClass("answered");
+
+            // add the selected class for highlighting
+            target.next().addClass("selected");
+
+            // loop thru all inputs and update their color
+            // add class to text that the user chose
+            for (let i = 0; i < labels.length; i++){
+                let label = labels.eq(i);
+                if (!label.prev().prop("checked")){ // if it's not checked, remove the class
+                    label.removeClass("selected");
+                }
+            }
         } 
+    });
+
+    $("input").on("change", (event) => {
+        let target = $(event.target);
+        // adds class to div so it's now answered
+        target.parent().addClass("answered");
+        console.log(target.val());
+
+    });
+
+    // for the drop downs
+    $("select").on("change", (event) => {
+        let target = $(event.target);
+        // adds class to div
+        if (target.val() == "---"){ // "---" is the default option, so if that is selected than the user has not answered
+            target.parent().removeClass("answered");
+            target.removeClass("selected");
+        } else {
+            target.parent().addClass("answered");
+            target.addClass("selected");
+        }
+
     });
 
 
