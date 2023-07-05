80b270252cd4ff4102b4b6ce62c84a91250e6b0b
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-04 20:41:10 -0700

    idk what changed but jic

--- P1/v03/cool.js
+++ P1/v03/cool.js
@@ -3,13 +3,14 @@ $(document).ready(()=>{
     let questions = $("form > p");
     let inputs = $("input[type='radio']");
 
+    // loop thru all inputs
     for (let i = 0; i < inputs.length; i++){
         let input = inputs.eq(i);
         console.log(input);
 
-        if (input.prop("checked")){
-            input.prev().addClass("answered");
-        }
+        // if (input.prop("checked")){
+        //     input.prev().addClass("answered");
+        // }
 
 
     }
