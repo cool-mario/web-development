2a86e7efca6ad7cbf2b8ac601ce13f90fc222698
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-04 23:31:01 -0700

    Add files via upload

---
+++ notes/14radioValidation.php
@@ -0,0 +1,19 @@
+<p>
+    <?php
+
+    // checks if a variable exists or not
+    if(isset($_POST["sword"])){
+        echo "you have a sword!";
+
+        $swords = array("life", "summoning", "fire", "among us");
+        if (in_array($_POST["sword"], $swords)){
+            echo "no cap that's fire bro!!! keep up the drip my g";
+        } else{
+            echo "You seem to have a crappy sword. L + ratio + don't care + you fell off";
+        }
+    }
+
+    
+
+    ?>
+</p>
