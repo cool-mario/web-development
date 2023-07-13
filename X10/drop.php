91dcc86beff3ae99f24ef7ae0cb06fe209325a82
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-12 22:44:26 -0700

    imma fall asleep

--- X10/drop.php
+++ X10/drop.php
@@ -6,6 +6,19 @@
     <title>parkamon dropping tables!!!!</title>
 </head>
 <body>
-    
+<?php
+
+require_once "../X9-The-Chalkboard-Manifesto/config.php";
+try {
+    $dbh = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
+    $query = file_get_contents('drop.sql');
+    $dbh->exec($query);
+    echo "<p>Successfully dropped databases!</p>";
+}
+catch (PDOException $e) {
+    echo "<p>Error: {$e->getMessage()}</p>";
+}
+
+?>
 </body>
 </html>
