38c0f6221224c71fa45aaad6bfff4db34c7f55e6
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-16 15:29:32 -0700

    update index and started project 2

---
+++ P2/shop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>The Shop store whatever idk</title>
+</head>
+<body>
+    <h1>idk what to name the shop store</h1>
+
+    <?php
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+
+    ?>    
+</body>
+</html>
