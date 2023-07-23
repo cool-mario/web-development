5d7f4ea99a76aae95eeb81f898b8dd8f1f8c75bf
Aidan Chan <50786617+cool-mario@users.noreply.github.com>
2023-07-22 21:35:35 -0700

    ‎

--- X10-Gotta-Coatch-em-All/config.php
+++ X10-Gotta-Coatch-em-All/config.php
@@ -2,6 +2,6 @@
 // Database Settings
 define('DB_DSN', 'mysql:host=localhost;dbname=acchan'); // change dbname to your username
 define('DB_USER', 'acchan');
-define('DB_PASSWORD', 'idk');
+define('DB_PASSWORD', 'UwU');
 
 ?>
